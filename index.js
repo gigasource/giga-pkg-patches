3ac2c2f95dad0f50f7d7505c8f2e76af64a4c624
transse04731 <triracle@gmail.com>
2020-03-03 14:25:09 +0700

    fix typo

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const mock = requrie('mock-require');
+const mock = require('mock-require');
 const path = require('path');
 
 mock(path.resolve(__dirname, '../pkg/dictonary/phantom.js'), './patches/phantom.js');
