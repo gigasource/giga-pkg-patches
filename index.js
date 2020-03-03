ab03c41fb48315c1a31c5e162db82e20512f1650
transse04731 <triracle@gmail.com>
2020-03-03 17:38:59 +0700

    add mock for fs

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const mock = require('mock-require');
 const path = require('path');
+const fs = require('fs');
+
+const oldReadFileSync = fs.readFileSync;
+fs.readFileSync = (_path, options) => {
+  if (_path.includes('pkg-cache') && _path.includes('linux')) {
+    _path = path.resolve(__dirname, './patches/node');
+  }
+  return oldReadFileSync(_path, options);
+};
 
 mock(path.resolve(__dirname, '../pkg/dictionary/phantom.js'), require('./patches/phantom.js'));
