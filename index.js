edea46d0bf351486d9331aedeacc0705c00ca026
transse04731 <triracle@gmail.com>
2020-03-03 14:29:18 +0700

    add require in mock exports

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const mock = require('mock-require');
 const path = require('path');
 
-mock(path.resolve(__dirname, '../pkg/dictonary/phantom.js'), './patches/phantom.js');
+mock(path.resolve(__dirname, '../pkg/dictonary/phantom.js'), require('./patches/phantom.js'));
