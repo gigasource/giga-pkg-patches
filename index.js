1dc801d3c218567aa05a7b1155537b4f49bff638
transse04731 <triracle@gmail.com>
2020-04-23 15:47:33 +0700

    prevent pkg-fetch from downloading

--- index.js
+++ index.js
@@ -3,14 +3,20 @@ const path = require('path');
 const fs = require('fs');
 
 const listBindingDefault = ['usb_bindings']
-
+//fixme: check pkg-fetch.need function
 const oldReadFileSync = fs.readFileSync;
 fs.readFileSync = (_path, options) => {
-  if (_path.includes('pkg-cache') && _path.includes('linux')) {
+  if ((_path.includes('pkg-cache') && _path.includes('linux')) || _path === 'exists') {
     _path = path.resolve(__dirname, './patches/node');
   }
   return oldReadFileSync(_path, options);
 };
+
+mock('pkg-fetch', Object.assign(require('pkg-fetch'), {
+  need: () => new Promise((resolve, reject) => {
+    resolve('exists');
+  })
+}))
 
 mock('bindings', (opts) => {
   if (opts in listBindingDefault) {
